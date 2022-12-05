9c403153a9f076848e0ba898715dd1277d6b3ce3
Elsie <48270378+Crystal-JJ@users.noreply.github.com>
2022-12-05 14:10:43 +0800

    Add files via upload

---
+++ DREAM/__init__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+"""
+#
+#
+
+# File Name: __init__.py
+# Description:
+
+"""
+
+__author__ = "Jing Jiang"
+__email__ = "jingdefengye1314@126.com"
+__version__ = '1.0.3'
+
+from .layer import *
+from .model import *
+from .loss import *
+
+
